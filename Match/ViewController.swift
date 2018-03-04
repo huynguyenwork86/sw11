d5d778a33de1d51db8ce031f0ce0f0ab2ff899e2
Huy Nguyen <huynguyenwork86@gmail.com>
2018-03-04 17:29:01 -0400

    lect1

--- Match/ViewController.swift
+++ Match/ViewController.swift
@@ -10,5 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    var flipCount = 0{
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    var emojiChoices = ["🎃", "👻", "🎃", "👻"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender){
+            chooseCard(emoji: emojiChoices[cardNumber], on: sender)
+        }
+        else{
+            print("error - selected card is not in the list")
+        }
+    }
+    
+    func chooseCard(emoji: String, on button: UIButton){
+        if(emoji == button.currentTitle){
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = UIColor.orange
+        }
+        else{
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = UIColor.white
+        }
+    }
 }
 
