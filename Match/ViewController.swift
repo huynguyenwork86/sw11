b378ca51196e44b2c93bea20d4f962ba1e57e87c
Huy Nguyen <huynguyenwork86@gmail.com>
2018-03-05 09:51:53 -0400

    extension

--- Match/ViewController.swift
+++ Match/ViewController.swift
@@ -53,8 +53,7 @@ class ViewController: UIViewController {
     }
     func emoji(for card: Card) -> String{
         if emoji[card.identifier] == nil, emojiChoices.count > 0{
-            let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
-            emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
+            emoji[card.identifier] = emojiChoices.remove(at:emojiChoices.count.arc4random)
         }
         return emoji[card.identifier] ?? "?"
     }
@@ -67,6 +66,19 @@ class ViewController: UIViewController {
         else{
             button.setTitle(emoji, for: UIControlState.normal)
             button.backgroundColor = UIColor.white
+        }
+    }
+}
+extension Int{
+    var arc4random: Int{
+        if self > 0{
+            return Int(arc4random_uniform(UInt32(self)))
+        }
+        else if self < 0 {
+            return Int(arc4random_uniform(UInt32(abs(self))))
+        }
+        else{
+            return 0
         }
     }
 }
